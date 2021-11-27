3a55288f367e8c68914cb821caef1540b166944c
Tholkappiyan <tholkappiyan@hypto.in>
2021-11-27 14:23:25 +0530

    Initial Commit

---
+++ lib/hws-connectors.rb
@@ -0,0 +1,8 @@
+module Hws
+  module Connectors # :nodoc:
+    VERSION = '0.1.0'
+  end
+end
+
+require 'hws-connectors/base'
+require 'hws-connectors/hypto/base'
---
+++ lib/hws-connectors/hypto/base.rb
@@ -0,0 +1,7 @@
+module Hws::Connectors::Hypto
+end
+
+class Hws::Connectors::Hypto::Base < Hws::Connectors::Base
+end
+
+require 'hws-connectors/hypto/payout'
