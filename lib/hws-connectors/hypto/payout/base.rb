be3f326fdf61039200ef557a18ea899c2e778660
ddlogesh <logeshdina@hypto.in>
2021-11-29 17:36:56 +0530

    Added Payout

--- lib/hws-connectors/hypto/payout/base.rb
+++ lib/hws-connectors/hypto/payout/base.rb
@@ -1,13 +1,13 @@
 class Hws::Connectors::Hypto::Payout < Hws::Connectors::Hypto
-  def send_to_bank_account(reference_number: nil, payment_type:, account_ifsc:, account_number:, amount:, beneficiary_note:, beneficiary_name: nil, options: {})
-    payload = { reference_number: reference_number, payment_type: payment_type, amount: amount, number: account_number, ifsc: account_ifsc,
-                note: beneficiary_note, udf1: options['udf1'], udf2: options['udf2'], udf3: options['udf3'], beneficiary_name: beneficiary_name }
+  def send_to_bank_account(reference_number: nil, account_number:, account_ifsc:, amount:, payment_type:, beneficiary_note:, beneficiary_name: nil, options: {})
+    payload = { reference_number: reference_number, number: account_number, ifsc: account_ifsc, amount: amount, payment_type: payment_type,
+                note: beneficiary_note, beneficiary_name: beneficiary_name, udf1: options['udf1'], udf2: options['udf2'], udf3: options['udf3'] }
     initiate_request(__method__, payload)
   end
 
   def send_to_upi_id(reference_number: nil, upi_id:, amount:, beneficiary_note:, beneficiary_name: nil, options: {})
-    payload = { reference_number: reference_number, payment_type: 'UPI', amount: amount, upi_id: upi_id,
-                note: beneficiary_note, udf1: options['udf1'], udf2: options['udf2'], udf3: options['udf3'], beneficiary_name: beneficiary_name }
+    payload = { reference_number: reference_number, upi_id: upi_id, amount: amount, payment_type: 'UPI',
+                note: beneficiary_note, beneficiary_name: beneficiary_name, udf1: options['udf1'], udf2: options['udf2'], udf3: options['udf3'] }
     initiate_request(__method__, payload)
   end
 
